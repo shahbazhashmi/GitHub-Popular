d103d72febe0943aa43803fe67131ce1b23c0850
Shahbaz Hashmi <shahbaz@DEV-386.local>
2020-03-08 11:43:24 +0530

    feat: added pull to refresh for repo list

--- app/src/main/java/org/gojek/github/ui/githubrepo/GithubRepoActivity.kt
+++ app/src/main/java/org/gojek/github/ui/githubrepo/GithubRepoActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.observe
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import org.gojek.github.R
 import org.gojek.github.databinding.ActivityGithubRepoBinding
 import org.gojek.github.ui.BaseActivity
+import org.gojek.github.utils.AppUtil
 import org.gojek.github.utils.extensions.getViewModel
 
-class GithubRepoActivity : BaseActivity() {
+
+class GithubRepoActivity : BaseActivity(), SwipeRefreshLayout.OnRefreshListener {
 
     private val TAG = "GithubRepoActivity"
 
@@ -23,10 +26,13 @@ class GithubRepoActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_github_repo)
         binding.vm = githubRepoViewModel
+        binding.swipeContainer.setOnRefreshListener(this)
         githubRepoViewModel.loaderHelper.setRetryListener {
             getGithubRepos()
         }
+
         getGithubRepos()
+
     }
 
     private fun getGithubRepos() {
@@ -56,5 +62,47 @@ class GithubRepoActivity : BaseActivity() {
                 }
             }
         }
+    }
+
+
+    private fun getGithubReposFromServer() {
+        /*
+        * Observing for data change from Network
+        * */
+        githubRepoViewModel.getGithubReposFromServer().observe(this) {
+            when {
+                it.status.isLoading() -> {
+                    binding.swipeContainer.isRefreshing = true
+                }
+                it.status.isSuccessful() -> {
+                    Log.d(TAG, "success")
+                    binding.swipeContainer.isRefreshing = false
+                    if (it.data == null || it.data!!.isEmpty()) {
+                        AppUtil.showToast(
+                            this,
+                            "${getString(R.string.txt_data_not_found)} - ${getString(R.string.txt_try_again_later)}",
+                            false
+                        )
+                    } else {
+                        githubRepoViewModel.loaderHelper.dismiss()
+                        githubRepoViewModel.githubRepoAdapter.setData(it.data!!)
+                    }
+                }
+                it.status.isError() -> {
+                    binding.swipeContainer.isRefreshing = false
+                    Log.d(TAG, it.errorMessage.toString())
+                    AppUtil.showToast(
+                        this,
+                        "${getString(R.string.txt_something_went_wrong)} - ${getString(R.string.txt_alien_blocking_signal)}",
+                        false
+                    )
+                }
+            }
+        }
+    }
+
+
+    override fun onRefresh() {
+        getGithubReposFromServer()
     }
 }
---
+++ app/src/main/java/org/gojek/github/utils/AppUtil.kt
@@ -0,0 +1,41 @@
+package org.gojek.github.utils
+
+import android.app.Activity
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.text.TextUtils
+import android.widget.Toast
+
+
+/**
+ * Created by Shahbaz Hashmi on 2020-03-08.
+ */
+object AppUtil {
+
+    fun showToast(
+        context: Context?,
+        message: String?,
+        longDuration: Boolean
+    ) {
+        try {
+            if (context is Activity && (context as Activity?)!!.isFinishing) {
+                return
+            }
+            if (TextUtils.isEmpty(message) || context == null) return
+            val handler = Handler(Looper.getMainLooper())
+            handler.post(
+                Toast.makeText(
+                    context.getApplicationContext(),
+                    message,
+                    if (longDuration) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
+                )::show
+            )
+        } catch (e: NullPointerException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+}
