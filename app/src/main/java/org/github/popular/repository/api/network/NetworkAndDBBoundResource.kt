7ccc77ceb376296d4834d99318b6427099d02d95
Shahbaz Hashmi <shahbaz@DEV-386.local>
2020-10-01 00:11:31 +0530

    done: code cleanup and repository

--- app/src/main/java/org/github/popular/repository/api/network/NetworkAndDBBoundResource.kt
+++ app/src/main/java/org/github/popular/repository/api/network/NetworkAndDBBoundResource.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.annotation.MainThread
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
@@ -31,36 +30,53 @@ abstract class NetworkAndDBBoundResource<ResultType, RequestType> {
     private val supervisorJob = SupervisorJob()
 
     suspend fun build(): NetworkAndDBBoundResource<ResultType, RequestType> {
-        withContext(Dispatchers.Main) { result.value =
-            Resource.loading()
+        withContext(Dispatchers.Main) {
+            result.value =
+                Resource.loading()
         }
         CoroutineScope(coroutineContext).launch(supervisorJob) {
-            val dbResult = loadFromDb()
-            if (shouldFetch(dbResult)) {
-                try {
-                    fetchFromNetwork()
-                } catch (e: Exception) {
-                    Log.e(TAG, "An error happened: $e")
-                    setValue(Resource.error(e.localizedMessage))
-                }
-            } else {
-                Log.d(TAG, "Return data from local database")
-                setValue(Resource.success(dbResult))
-            }
+            handleData()
         }
         return this
+    }
+
+    private suspend fun handleData() {
+        val dbResult = loadFromDb()
+        if (mustFetch() || shouldFetch(dbResult)) {
+            try {
+                val apiResponse = fetchFromNetwork()
+                if (apiResponse.status.isSuccessful()) {
+                    saveCallResults(processResponse(apiResponse))
+                    /**
+                     * publish new data
+                     */
+                    setValue(Resource.success(loadFromDb()))
+                } else {
+                    throw Exception(apiResponse.errorMessage ?: "something went wrong")
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "An error happened: $e")
+                if (mustFetch() || !isLocalDataAvailable(dbResult)) {
+                    setValue(Resource.error(e.message ?: "something went wrong"))
+                } else {
+                    setValue(Resource.success(dbResult))
+                }
+            }
+        } else {
+            Log.d(TAG, "Return data from local database")
+            setValue(Resource.success(dbResult))
+        }
     }
 
     fun asLiveData() = result as LiveData<Resource<ResultType>>
 
     // ---
 
-    private suspend fun fetchFromNetwork() {
+    private suspend fun fetchFromNetwork(): Resource<RequestType> {
         Log.d(NetworkAndDBBoundResource::class.java.name, "Fetch data from network")
         val apiResponse = createCall()
         Log.e(NetworkAndDBBoundResource::class.java.name, "Data fetched from network")
-        saveCallResults(processResponse(apiResponse))
-        setValue(Resource.success(loadFromDb()))
+        return apiResponse
     }
 
     @MainThread
@@ -79,115 +95,15 @@ abstract class NetworkAndDBBoundResource<ResultType, RequestType> {
     protected abstract fun shouldFetch(data: ResultType?): Boolean
 
     @MainThread
+    protected abstract fun mustFetch(): Boolean
+
+    @MainThread
+    protected abstract fun isLocalDataAvailable(data: ResultType?): Boolean
+
+    @MainThread
     protected abstract suspend fun loadFromDb(): ResultType
 
     @MainThread
     protected abstract suspend fun createCall(): Resource<RequestType>
 
-   /* *//**
-     * The final result LiveData
-     *//*
-    private val result = MediatorLiveData<Resource<ResultType?>>()
-
-    init {
-        // Send loading state to UI
-        result.value = Resource.loading()
-
-        val dbSource = this.loadFromDb()
-
-        result.addSource(dbSource) { data ->
-
-            result.removeSource(dbSource) // Once done data loading remove source
-
-            if (mustFetch(data)) {
-
-                fetchFromNetwork(dbSource, true)
-
-            } else if (shouldFetch(data)) {
-
-                fetchFromNetwork(dbSource, false)
-
-            } else {
-
-                result.addSource(dbSource) { newData -> setValue(Resource.success(newData)) }
-            }
-        }
-    }
-
-    *//**
-     * Fetch the data from network and persist into DB and then
-     * send it back to UI.
-     *//*
-    private fun fetchFromNetwork(dbSource: LiveData<ResultType>, mustFetch: Boolean) {
-        val apiResponse = createCall()
-        // we re-attach dbSource as a new source, it will dispatch its latest value quickly
-        result.addSource(dbSource) {
-            result.setValue(Resource.loading())
-        }
-
-        result.addSource(apiResponse) { response ->
-            result.removeSource(dbSource)
-            result.removeSource(apiResponse)
-
-            response?.also { data ->
-                if (data.status.isSuccessful()) {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        processResponse(data)?.let { requestType ->
-                            saveCallResult(requestType)
-                        }
-                        withContext(Dispatchers.Main) {
-                            // we specially request a new live data,
-                            // otherwise we will get immediately last cached value,
-                            // which may not be updated with latest results received from network.
-                            result.addSource(loadFromDb()) { newData ->
-                                setValue(Resource.success(newData))
-                            }
-                        }
-                    }
-                } else {
-                    onFetchFailed()
-                    result.addSource(if (mustFetch) apiResponse else dbSource) {
-                        result.setValue(Resource.error(data.errorMessage))
-                    }
-                }
-            }
-        }
-    }
-
-    @MainThread
-    private fun setValue(newValue: Resource<ResultType?>) {
-        if (result.value != newValue) result.value = newValue
-    }
-
-    protected fun onFetchFailed() {}
-
-    fun asLiveData(): LiveData<Resource<ResultType?>> {
-        return result
-    }
-
-    @WorkerThread
-    private fun processResponse(response: Resource<RequestType>): RequestType? {
-        return response.data
-    }
-
-    @WorkerThread
-    protected abstract fun saveCallResult(item: RequestType)
-
-    *//**
-     * prefers network call and does not return network call errors
-     *//*
-    @MainThread
-    protected abstract fun shouldFetch(data: ResultType?): Boolean
-
-    *//**
-     * prefers network call and not returns network call errors
-     *//*
-    @MainThread
-    protected abstract fun mustFetch(data: ResultType?): Boolean
-
-    @MainThread
-    protected abstract fun loadFromDb(): LiveData<ResultType>
-
-    @MainThread
-    protected abstract fun createCall(): LiveData<Resource<RequestType>>*/
 }
--- app/src/main/java/org/github/popular/repository/repo/GithubRepoRepository.kt
+++ app/src/main/java/org/github/popular/repository/repo/GithubRepoRepository.kt
@@ -1,13 +1,7 @@
 package org.github.popular.repository.repo
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.github.popular.repository.api.ApiServiceHelper
 import org.github.popular.repository.api.network.NetworkAndDBBoundResource
 import org.github.popular.repository.api.network.Resource
@@ -33,43 +27,13 @@ class GithubRepoRepository @Inject constructor(
      */
     suspend fun getGithubRepos(callApiForcefully: Boolean): LiveData<Resource<List<GithubRepo>?>> {
 
-        /*return object :
-            NetworkAndDBBoundResource<List<GithubRepo>, List<GithubRepo>>() {
-            override fun saveCallResult(item: List<GithubRepo>) {
-                if (!item.isEmpty()) {
-                    sharedPreferenceManager.setLastUpdatedTimestamp()
-                    CoroutineScope(Dispatchers.Main).launch {
-                        githubRepoDbHelper.deleteAllRepos()
-                        githubRepoDbHelper.insertRepos(item)
-                    }
-                }
-            }
-
-            override fun mustFetch(data: List<GithubRepo>?) = callApiForcefully
-
-            override fun shouldFetch(data: List<GithubRepo>?): Boolean {
-                if (ConnectivityUtil.isConnected(context) && sharedPreferenceManager.isLocalDataExpired()) {
-                    return true
-                }
-                return false
-            }
-
-            override fun loadFromDb() = liveData(Dispatchers.Main) {
-                emit(githubRepoDbHelper.getAllRepos())
-            }
-
-            override fun createCall() = apiServiceHelper.getRepos()
-
-
-        }.asLiveData()*/
-
         return object :
             NetworkAndDBBoundResource<List<GithubRepo>?, List<GithubRepo>>() {
             override suspend fun saveCallResults(items: List<GithubRepo>?) {
-                if (items != null && !items.isEmpty()) {
+                if (items != null && items.isNotEmpty()) {
                     sharedPreferenceManager.setLastUpdatedTimestamp()
-                        githubRepoDbHelper.deleteAllRepos()
-                        githubRepoDbHelper.insertRepos(items)
+                    githubRepoDbHelper.deleteAllRepos()
+                    githubRepoDbHelper.insertRepos(items)
 
                 }
             }
@@ -78,15 +42,21 @@ class GithubRepoRepository @Inject constructor(
                 if (ConnectivityUtil.isConnected(context) && sharedPreferenceManager.isLocalDataExpired()) {
                     return true
                 }
-                if(data == null || data.isEmpty()) {
+                if (!isLocalDataAvailable(data)) {
                     return true
                 }
                 return false
             }
 
+            override fun mustFetch(): Boolean = callApiForcefully
+
+            override fun isLocalDataAvailable(data: List<GithubRepo>?): Boolean =
+                data != null && data.isNotEmpty()
+
             override suspend fun loadFromDb(): List<GithubRepo> = githubRepoDbHelper.getAllRepos()
 
-            override suspend fun createCall(): Resource<List<GithubRepo>> = apiServiceHelper.getRepos()
+            override suspend fun createCall(): Resource<List<GithubRepo>> =
+                apiServiceHelper.getRepos()
         }.build().asLiveData()
     }
 
